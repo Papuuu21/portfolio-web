9fb57c3bb6f47fbf908fd5554319cca8010ad08b
PapudeAlva <dealva.pablo@gmail.com>
2024-06-11 20:40:30 +0200

    Añado reflex run a build.sh porque no conecta al servidor

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
+reflex run --frontend-only
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
