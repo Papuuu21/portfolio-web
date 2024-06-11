0f4f3fd06fab742d82ba56979f0ca69e755adc2d
PapudeAlva <dealva.pablo@gmail.com>
2024-06-11 21:18:35 +0200

    Correccion de build.sh quitando reflex run

--- build.sh
+++ build.sh
@@ -5,7 +5,6 @@ python -m pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-reflex run 
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
